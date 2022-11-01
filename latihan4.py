aec16037601a0c631e260af6c3b44a7f9dd3bc39
sintahardianti <115516473+sintahardianti@users.noreply.github.com>
2022-11-01 10:41:32 +0700

    Add files via upload

---
+++ latihan4.py
@@ -0,0 +1,6 @@
+print('Menghitung luas lingakaran')
+print('______________________________')
+r = int(input('masukan jari-jari lingkaran: '))
+phi = 3.14
+L = phi * (r * r)
+print('Luas lingakaran dengan jari-jari {} adalah {}'.format(r, L))
