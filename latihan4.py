38b204fa450ac3cae36137505216b293397a1b9e
sintahardianti <115516473+sintahardianti@users.noreply.github.com>
2022-11-01 10:45:43 +0700

    Delete latihan4.py

--- latihan4.py
+++
@@ -1,6 +0,0 @@
-print('Menghitung luas lingakaran')
-print('______________________________')
-r = int(input('masukan jari-jari lingkaran: '))
-phi = 3.14
-L = phi * (r * r)
-print('Luas lingakaran dengan jari-jari {} adalah {}'.format(r, L))
